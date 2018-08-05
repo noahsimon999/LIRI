d982dc19e6d747a508df4ef8490577271020c017
nohabroha <noahysimon@gmail.com>
2018-08-05 17:03:10 -0500

    all working

--- liri.js
+++ liri.js
@@ -14,8 +14,18 @@ var client = new Twitter({
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+var fs = require("fs");
 
+
+fs.appendFile("log.txt", process.argv[2] + " " + process.argv[3] + "\n", function(err) {
+    if (err) {
+      console.log(err);
+    }
+});
+
+// Switch controller
 switch (process.argv[2]) {
+// twitter api
     case 'my-tweets': 
         var params = {screen_name: process.argv[3]};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -26,8 +36,15 @@ switch (process.argv[2]) {
             }
         });
     break;
+// spotify api
     case 'spotify-this-song': 
-        spotify.search({ type: 'track', query: process.argv[3], limit:1 }, function(err, data) {
+        var query = "";
+        if (process.argv[3] === undefined) {
+            query = "The Sign";
+        } else {
+            query = process.argv[3];
+        }
+        spotify.search({ type: 'track', query: query, limit:1 }, function(err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
@@ -37,6 +54,7 @@ switch (process.argv[2]) {
             console.log("Song Url: " + data.tracks.items[0].external_urls.spotify);
         });
     break;
+//OMDB api
     case 'movie-this': 
         if (process.argv[3] === undefined) {
             var queryUrl = "http://www.omdbapi.com/?t=Mr-Nobody&y=&plot=short&apikey=trilogy";
@@ -58,7 +76,32 @@ switch (process.argv[2]) {
             }
         });
     break;
-    case 'do-what-it-says': 
-        console.log(client);
+// Random call
+    case 'do-what-it-says':
+        fs.readFile("random.txt", "utf8", function(error, data) {
+            if(error) {
+                return console.log(error);
+            }
+            console.log(data);
+            
+            spotify.search({ type: 'track', query: data, limit:1 }, function(err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                console.log("Artist's Name: " + data.tracks.items[0].album.artists[0].name);
+                console.log("Album Name: " + data.tracks.items[0].album.name);
+                console.log("Song Name: " + data.tracks.items[0].name);
+                console.log("Song Url: " + data.tracks.items[0].external_urls.spotify);
+            });
+
+
+
+        })
+    
+
+
+
+
+
     break;
 }
